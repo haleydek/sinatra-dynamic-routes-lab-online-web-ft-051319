c264ba930a8ecb0606eb0092dd0353cd936f2269
Haley DeKeyser <hcd09@live.com>
2019-06-24 22:40:11 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -29,6 +29,6 @@ class App < Sinatra::Base
     @operation = params[:operation]
     @number1 = params[:number1].to_i
     @number2 = params[:number2].to_i
-    (@number1 @operation @number2).to_s
+    @operation(@number1, @number2).to_s
   end
 end
