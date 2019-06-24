19f73c25b7bf71cd871e0b2576d275f934dda118
Haley DeKeyser <hcd09@live.com>
2019-06-24 22:39:26 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -26,10 +26,9 @@ class App < Sinatra::Base
   end
   
   get '/:operation/:number1/:number2' do
+    @operation = params[:operation]
     @number1 = params[:number1].to_i
     @number2 = params[:number2].to_i
-    (@number1 - @number2).to_s
-    (@number1 * @number2).to_s
-    (@number1 / @number2).to_s
+    (@number1 @operation @number2).to_s
   end
 end
