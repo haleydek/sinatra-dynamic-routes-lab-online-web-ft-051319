76a384a4cb1ddeb80da0625996bfacdac3949d21
Haley DeKeyser <hcd09@live.com>
2019-06-24 22:27:50 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -13,6 +13,6 @@ class App < Sinatra::Base
   get '/say/:number/:phrase' do
     @number = params[:number].to_i
     @phrase = params[:phrase]
-    @number.times{@phrase}
+    @phrase * @number
   end
 end
